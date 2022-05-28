e9f4f1f5ef80f959a3e35ce171ae099c15eec6b7
luser <2802458691@qq.com>
2022-05-28 10:26:58 +0800

    条形图呈现

--- gihub上星级最高的项目查找.py
+++ gihub上星级最高的项目查找.py
@@ -1,27 +1,51 @@
 import requests
 from plotly.graph_objs import Bar
 from plotly import offline
-#执行API调用并响应
-url='https://api.github.com/search/repositories?q=language:python&sort=stars'
-headers = {'Accept':'application/vnd.github.v3+json'}
-r = requests.get(url,headers=headers)
+
+# 执行API调用并响应
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+headers = {'Accept': 'application/vnd.github.v3+json'}
+r = requests.get(url, headers=headers)
 print(f"Status code:{r.status_code}")
-#姜API响应赋值给一个变量
 response_dict = r.json()
-print(f"Total repositories: {response_dict['total_count']}")
-#探索仓库有关信息
 repo_dicts = response_dict['items']
-print(f"Repositories returned: {len(repo_dicts)}")
-
-#研究第一个仓库
-repo_dict = repo_dicts[0]
+repo_links, stars, labels = [], [], []
 for repo_dict in repo_dicts:
-    print("\n有关每一个库的指定信息:")
-    print(f"名称：{repo_dict['name']}")
-    print(f"所有者：{repo_dict['owner']['login']}")
-    print(f"星数：{repo_dict['stargazers_count']}")
-    print(f"仓库网址：{repo_dict['html_url']}")
-    print(f"创建时间：{repo_dict['created_at']}")
-    print(f"更新时间：{repo_dict['updated_at']}")
-    print((f"仓库描述：{repo_dict['description']}"))
+    repo_name = repo_dict['name']
+    repo_url = repo_dict['html_url']
+    repo_link = f"<a href='{repo_url}'>{repo_name}</a>"
+    repo_links.append(repo_link)
+    stars.append(repo_dict['stargazers_count'])
 
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}"
+    labels.append(label)
+# 可视化
+data = [{
+    'type': 'bar',
+    'x': repo_links,
+    'y': stars,
+    'hovertext': labels,
+    'marker': {
+        'color': 'rgb(60,100,150)',
+        'line': {'width': 1.5, 'color': 'rgb(25,25,25)'}
+    },
+    'opacity': 0.6,
+}]
+my_layout = {
+    'title': 'GitHub上最受欢迎的Python项目',
+    'titlefont': {'size': 28},
+    'xaxis': {'title': '仓库',
+              'titlefont': {'size': 24},
+              'tickfont': {'size': 14},
+              },
+    'yaxis': {'title': '星数',
+              'titlefont': {'size': 24},
+              'tickfont': {'size': 14},
+              },
+
+}
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='Python_repo.html')
